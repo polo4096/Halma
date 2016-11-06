63010a6eaf2038cd185730744a665e464184e674
Pierre Ghazal <pghazal-ext@lequipe.fr>
2016-11-07 00:26:18 +0100

    Affichage tableau 2 dim

--- main.c
+++ main.c
@@ -3,6 +3,37 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int n = 10;
+    int m = 10;
+
+    char*** tab = (char***) malloc(n * sizeof(char**));
+    int i = 0;
+    for(i = 0; i < n; i++) {
+        tab[i] = (char**) malloc(m * sizeof(char*));
+    }
+
+    i = 0;
+    int j = 0;
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < m; j++) {
+            if(j % 2 == 0) {
+                tab[i][j] = "|";
+            } else {
+                if(i % 2 == 0) {
+                    tab[i][j] = " * ";
+                } else {
+                    tab[i][j] = "---";
+                }
+            }
+        }
+        tab[i][m-1] = "\n";
+    }
+
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < m; j++) {
+            printf("%s", tab[i][j]);
+        }
+    }
     return 0;
 }
